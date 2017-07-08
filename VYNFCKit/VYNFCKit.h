5ae8bffbf55c18181294fc7d8d4ef6ea759c2a1f
Vince Yuan <vince.yuan@gmail.com>
2017-07-08 13:14:25 +0800

    Initial Commit

---
+++ VYNFCKit/VYNFCKit.h
@@ -0,0 +1,19 @@
+//
+//  VYNFCKit.h
+//  VYNFCKit
+//
+//  Created by Vince Yuan on 7/8/17.
+//  Copyright © 2017 Vince Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VYNFCKit.
+FOUNDATION_EXPORT double VYNFCKitVersionNumber;
+
+//! Project version string for VYNFCKit.
+FOUNDATION_EXPORT const unsigned char VYNFCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VYNFCKit/PublicHeader.h>
+
+
