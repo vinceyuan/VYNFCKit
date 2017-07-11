6b81378d06566dce811a88c6a92ba14ce3652b85
Vince Yuan <vince.yuan@gmail.com>
2017-07-12 01:11:50 +0800

    Read NFC tag in VYNFCKitExampleObjc.

--- Examples/VYNFCKitExampleObjc/VYNFCKitExampleObjc/ViewController.m
+++ Examples/VYNFCKitExampleObjc/VYNFCKitExampleObjc/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <CoreNFC/CoreNFC.h>
+#import <VYNFCKit/VYNFCKit.h>
 
-@interface ViewController ()
+@interface ViewController () <NFCNDEFReaderSessionDelegate> {
+    VYNFCNDEFPayloadParser *_parser;
+}
 
 @end
 
@@ -16,7 +20,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _parser = [[VYNFCNDEFPayloadParser alloc] init];
 }
 
 
@@ -26,8 +30,26 @@
 }
 
 - (IBAction)didTapScanNFCTagButton:(id)sender {
-    
+    NFCNDEFReaderSession *session = [[NFCNDEFReaderSession alloc] initWithDelegate:self queue:dispatch_get_main_queue() invalidateAfterFirstRead:NO];
+    [session beginSession];
 }
+
+#pragma mark - NFCNDEFReaderSessionDelegate
+
+- (void)readerSession:(nonnull NFCNDEFReaderSession *)session didDetectNDEFs:(nonnull NSArray<NFCNDEFMessage *> *)messages {
+    for (NFCNDEFMessage *message in messages) {
+        for (NFCNDEFPayload *payload in message.records) {
+            VYNFCNDEFPayload *parsedPayload = [_parser parse:payload];
+            if (parsedPayload) {
+                NSLog(@"%@", parsedPayload.text);
+            }
+        }
+    }
+}
+
+- (void)readerSession:(nonnull NFCNDEFReaderSession *)session didInvalidateWithError:(nonnull NSError *)error {
+}
+
 
 
 @end
