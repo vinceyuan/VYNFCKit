854dfbb6b6492a2cf9946e1c10b6686fda493212
Vince Yuan <vince.yuan@gmail.com>
2017-07-20 01:34:01 +0800

    Update license.

--- VYNFCKit/VYNFCNDEFPayloadTypes.m
+++ VYNFCKit/VYNFCNDEFPayloadTypes.m
@@ -5,6 +5,9 @@
 //  Created by Vince Yuan on 7/14/17.
 //  Copyright © 2017 Vince Yuan. All rights reserved.
 //
+//  This source code is licensed under the MIT-style license found in the
+//  LICENSE file in the root directory of this source tree.
+//
 
 #import "VYNFCNDEFPayloadTypes.h"
 
