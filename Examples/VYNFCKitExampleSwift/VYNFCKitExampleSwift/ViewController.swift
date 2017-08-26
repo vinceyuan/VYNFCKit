f8b80b066fe10e23ee3e49820a02a109cacd719b
Vince Yuan <vince.yuan@gmail.com>
2017-08-26 10:35:00 +0800

    Add empty VYNFCKitExampleSwift project.

---
+++ Examples/VYNFCKitExampleSwift/VYNFCKitExampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VYNFCKitExampleSwift
+//
+//  Created by Vince Yuan on 8/26/17.
+//  Copyright © 2017 Vince Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
