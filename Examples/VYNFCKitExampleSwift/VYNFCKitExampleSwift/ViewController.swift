28650123850e2e2cfcb15a96078a21dce0dabf0f
Vince Yuan <vince.yuan@gmail.com>
2017-08-26 14:48:56 +0800

    Implement scanning NFC in swift sample project.

--- Examples/VYNFCKitExampleSwift/VYNFCKitExampleSwift/ViewController.swift
+++ Examples/VYNFCKitExampleSwift/VYNFCKitExampleSwift/ViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import CoreNFC
+import VYNFCKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+
+    var results: String = ""
+    @IBOutlet weak var textViewResults: UITextView!
+    @IBOutlet weak var switchOpenURI: UISwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,61 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didTapScanNFCTagButton(_ sender: Any) {
+        results = ""
+        textViewResults.text = ""
+        let session = NFCNDEFReaderSession(delegate: self, queue: DispatchQueue.main, invalidateAfterFirstRead: false)
+        session.begin()
+    }
+
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        for message in messages {
+            for payload in message.records {
+                guard let parsedPayload = VYNFCNDEFPayloadParser.parse(payload) else {
+                    continue
+                }
+                var text = ""
+                var urlString = ""
+                if let parsedPayload = parsedPayload as? VYNFCNDEFTextPayload {
+                    text = "[Text payload]\n"
+                    text = String(format: "%@%@", text, parsedPayload.text)
+                } else if let parsedPayload = parsedPayload as? VYNFCNDEFURIPayload {
+                    text = "[URI payload]\n"
+                    text = String(format: "%@%@", text, parsedPayload.uriString)
+                    urlString = text
+                } else if let parsedPayload = parsedPayload as? VYNFCNDEFTextXVCardPayload {
+                    text = "[TextXVCard payload]\n"
+                    text = String(format: "%@%@", text, parsedPayload.text)
+                } else if let sp = parsedPayload as? VYNFCNDEFSmartPosterPayload {
+                    text = "[SmartPoster payload]\n"
+                    for textPayload in sp.payloadTexts {
+                        if let textPayload = textPayload as? VYNFCNDEFTextPayload {
+                            text = String(format: "%@%@\n", text, textPayload.text)
+                        }
+                    }
+                    text = String(format: "%@%@", text, sp.payloadURI.uriString)
+                    urlString = sp.payloadURI.uriString
+                } else if let wifi = parsedPayload as? VYNFCNDEFWifiSimpleConfigPayload {
+                    text = String(format: "%@SSID: %@\nPassword: %@\nMac Address: %@\nAuth Type: %@\nEncrypt Type: %@",
+                                  text, wifi.credential.ssid, wifi.credential.networkKey, wifi.credential.macAddress,
+                                  VYNFCNDEFWifiSimpleConfigCredential.authTypeString(wifi.credential.authType),
+                                  VYNFCNDEFWifiSimpleConfigCredential.encryptTypeString(wifi.credential.encryptType)
+                    )
+                    if let version2 = wifi.version2 {
+                        text = String(format: "%@\nVersion2: %@", text, version2.version)
+                    }
+                } else {
+                    text = "Parsed but unhandled payload type"
+                }
+                print(text)
+
+            }
+        }
+    }
+
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print(error)
+    }
 
 }
 
